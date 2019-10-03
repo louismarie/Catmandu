09f7840fe022566cc32a63bbec431a5f4f8a1574
louismarie <12646487+louismarie@users.noreply.github.com>
2019-10-03 15:58:29 +0200

    add doker-compose elasticsearch

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-time docker build . -t librecat/catmandu
-docker run -v /Users/admin/git/msw/polaris-os/polaris-os-oai/Catmandu/conf:/home/catmandu -it librecat/catmandu
+#time docker build . -t librecat/catmandu
+docker run -p 3000:3000 --network=catmandu_default -v /Users/admin/git/msw/polaris-os/polaris-os-oai/Catmandu/conf:/home/catmandu -it librecat/catmandu
 
-# catmandu import OAI --url https://lib.ugent.be/oai --metadataPrefix oai_dc --set flandrica --handler oai_dc to Elasticsearch --index_name oai --bag publication
+# catmandu import OAI --url https://lib.ugent.be/oai --metadataPrefix oai_dc --set flandrica --handler oai_dc to Elasticsearch --index_name oai --bag publication --nodes elasticsearch:9200
